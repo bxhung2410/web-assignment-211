ae5b31565bb44387196c888a6a4e41fc67182fbc
Bùi Xuân Hùng <79247401+bxhung2410@users.noreply.github.com>
2021-11-19 22:00:02 +0700

    Delete mvc directory

--- mvc/controllers/News.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class News extends Controller{
-        function index(){
-            $index = $this->view("application/Home/index");
-            $layout = $this->view("layouts/application", ["page"=>$index]);
-            echo $layout;
-        }
-    }
-?>
